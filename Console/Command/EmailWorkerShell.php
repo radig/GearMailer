205ef99e5ee01ae8058fea9bf7fdc535500696d7
Rafael Saeki Barbosa <ssaeki@gmail.com>
2013-07-12 10:59:09 -0400

    SesTransport to send emails via AmazonSes

---
+++ Console/Command/EmailWorkerShell.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Shell to start Gearman Worker
+ */
+App::uses('Shell', 'Console');
+App::uses('SendEmailWorker', 'GearMailer.Lib');
+
+class EmailWorkerShell extends Shell {
+    public $EmailWorker;
+
+/**
+ * Override startup
+ *
+ * @return void
+ */
+    public function startup()
+    {
+        parent::startup();
+        $this->out(__d('GearmanWorker', 'Cake GearmanWorker Shell'));
+        $this->hr();
+
+        $this->EmailWorker = new SendEmailWorker();
+    }
+
+/**
+ * Override main
+ *
+ * @return void
+ */
+    public function main()
+    {
+        $this->EmailWorker->init();
+    }
+}
---
+++ Lib/Network/Email/SesTransport.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * GearTransport and Gearman Client
+ */
+require APP . '/vendor/autoload.php';
+
+App::uses('AbstractTransport', 'Network/Email');
+use Aws\Ses\SesClient;
+
+class SesTransport extends AbstractTransport {
+
+    public function send(CakeEmail $cakeEmail) {
+        $config['key'] = $this->_config['username'];
+        $config['secret'] = $this->_config['password'];
+        $config['region'] = $this->_config['region'];
+
+        $sesEmail = SesClient::factory($config);
+
+        $headers = $cakeEmail->getHeaders(['from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'subject']);
+        $headers = $this->_headersToString($headers);
+
+        $lines = $cakeEmail->message();
+        $messages = [];
+        foreach ($lines as $line) {
+            if ((!empty($line)) && ($line[0] === '.')) {
+                $messages[] = '.' . $line;
+            } else {
+                $messages[] = $line;
+            }
+        }
+
+        $message = implode("\r\n", $messages);
+        $raw_message = $headers . "\r\n\r\n" . $message . "\r\n\r\n\r\n";
+
+        try {
+            $sesEmail->sendRawEmail(['RawMessage' => ['Data' => base64_encode($raw_message)]]);
+        } catch (MessageRejectedException $e) {
+            CakeLog::write('assync_mail', print_r($e->getMessage(),true));
+            return false;
+        }
+
+        return ['headers' => $headers, 'message' => $message];
+    }
+}
