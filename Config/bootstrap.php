7d84ec167f5682e2e1a3c761e6539db7a854976a
Cauan Cabral <cauan@radig.com.br>
2013-07-17 15:36:52 -0400

    Fixing Logs configuration

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,9 +3,11 @@ App::uses('CakeLog', 'Log');
 CakeLog::config('assync_job', array(
     'engine' => 'FileLog',
     'file' => 'assync_job',
+    'scopes' => ['worker_error']
 ));
 
 CakeLog::config('assync_mail', array(
     'engine' => 'FileLog',
     'file' => 'assync_mail',
+    'scopes' => ['mail_error']
 ));
