058a0d1b9c075875d4e43bb12fb73a5ef92ac3fb
Cauan Cabral <cauan@radig.com.br>
2013-09-09 16:57:52 -0400

    Included a php script to run service without CakePHP
    Included instructions to using Systemd as process manager for Worker

---
+++ Vendor/gearman_email_worker.php
@@ -0,0 +1,8 @@
+<?php
+require(__DIR__ . '/EmailWorker.php');
+
+$w = new EmailWorker();
+
+echo "Started at ", date('Y-m-d H:i:s'), "\n";
+$w->init();
+echo "Finished at ", date('Y-m-d H:i:s'), "\n";
